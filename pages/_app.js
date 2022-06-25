a3b5286fddcc874d26db8144d41fe9292d6d7125
Vanessa <vanessa.mercado24@gmail.com>
2022-06-25 10:43:08 -0400

    wip - material ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,9 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  const layoutStyle = `
 
-  `
-
-  const HomePageStyle = `
-  
-  `
   return (
-    <div className={layoutStyle}>
+    <div>
       <Component {...pageProps} />
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,47 @@ import Profiles from './profiles'
 
 export default function Home() {
 
-  const layoutStyle = `
-  bg-lens-100
-  h-screen
-  flex
-  `
+  const Layout = ({ children }) => {
+    const layoutStyle = `
+    bg-lens-100
+    h-screen
+    `
 
 
-  const buttonStyle = `
-  bg-lens-200
-  hover:bg-green-900
-  text-white
-  py-2
-  px-4
-  rounded-full
-  `
+    return (
+      <div className={layoutStyle}>
+        <main>{children}</main>
+      </div>
+    )
+  }
+
+  const Login = () => {
+    const loginStyle = `
+    bg-red-100
+    flex
+    `
+
+    return (
+      <div className={loginStyle}>
+        <h1>Log in</h1>
+        <button>Click me</button>
+      </div>
+    )
+  };
+
+  // const buttonStyle = `
+  // bg-lens-200
+  // hover:bg-green-900
+  // text-white
+  // py-2
+  // px-4
+  // rounded-full
+  // `
   return (
-    <div className={layoutStyle}>
-      <button className={buttonStyle}>Connect Wallet</button>
-    </div>
+    <>
+      <Layout>
+        <Login />
+      </Layout>
+    </>
   )
 }
